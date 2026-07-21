4fafc36ed7199ed10fad81684a569f978a82d7f9
unknown <bintangpratama10604-cloud>
2026-07-21 16:12:13 +0800

    cizycake

--- hapus_keranjang.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'koneksi.php';
-if(!isset($_SESSION['id'])){ header("Location: login.php"); exit; }
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM keranjang WHERE id=$id AND id_user=".$_SESSION['id']);
-header("Location: keranjang.php");
-?>
--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-$conn = mysqli_connect("localhost","root","","cizycake");
-if (!$conn) { die("Koneksi gagal: " . mysqli_connect_error()); }
-define('NAMA_TOKO', 'CiZycake');
-define('WA_ADMIN', '6281234567890');
-?>
